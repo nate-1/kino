8e6be29ef243fcde19b71c028e6a72f284f1d7d9
Nathan Wurtz <nathan.wurtz01@gmail.com>
2021-11-26 09:38:00 +0100

    Create project Kino.Services

---
+++ Kino.Services/MoviesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Kino.Data;
+
+namespace Kino.Services
+{
+    public class MoviesService
+    {
+       private readonly AppDbContext _appDbContext;
+
+       public MoviesService(AppDbContext appDbContext)
+       {
+           this._appDbContext = appDbContext;
+       }
+        
+    }
+}
